3f1f4b82e39a56c36cbde35c8cbc7fe8f77ffd4d
Wikub <wikub@users.noreply.github.com>
2022-03-04 15:41:53 +0100

    Update UserController.php
    PHP CS Fixer recommandation

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -140,7 +140,7 @@ class UserController extends AbstractController
     {
         $length = 15;
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#!?-&@';
-        $charactersLength = \mb_strlen($characters);
+        $charactersLength = mb_strlen($characters);
         $randomString = '';
         for ($i = 0; $i < $length; ++$i) {
             $randomString .= $characters[rand(0, $charactersLength - 1)];

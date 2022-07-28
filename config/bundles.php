d309efcf34a3ce78ee6758067e2aaee55d00ad82
Wikub <contact@wikub.com>
2022-07-28 10:02:42 +0200

    add fixture-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];

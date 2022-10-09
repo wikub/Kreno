1f265c4b7ebf6e715971a26edf20be1ce40763f2
Wikub <contact@wikub.com>
2022-10-09 18:31:37 +0200

    Add Service ti get Param

---
+++ src/Service/GetParam.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Param;
+use App\Repository\ParamRepository;
+
+class GetParam
+{
+    private ParamRepository $paramRepository;
+
+    public function __construct(ParamRepository $paramRepository)
+    {
+        $this->paramRepository = $paramRepository;
+    }
+
+    public function get(string $code): string
+    {
+        $param = $this->paramRepository->findOneBy(['code' => $code]);
+
+        if( !isset($param) ) return '';
+
+        return $param->getValue();
+    }
+}

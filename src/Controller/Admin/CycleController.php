ababc01f9fa30027f66fea4ddb9e8a103b984c90
Wikub <contact@wikub.com>
2022-11-08 11:11:56 +0100

    Cycle : move cycle genrator

--- src/Controller/Admin/CycleController.php
+++ src/Controller/Admin/CycleController.php
@@ -13,8 +13,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Cycle;
 use App\Repository\CycleRepository;
+use App\Repository\SettingRepository;
 use App\Service\CommitmentContratDebitLogApply;
+use App\Service\CycleGenerator;
+use App\Service\GetParam;
 use App\Service\Notification\CycleStartNotification;
+use DateTime;
+use DateTimeImmutable;
 use Exception;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -88,4 +93,55 @@ class CycleController extends AbstractController
         return $this->redirectToRoute('admin_cycle_index');   
     }
 
+    /**
+     * @Route("/week/gen", name="week_generator")
+     */
+    public function weekGenerator(
+        Request $request, 
+        CycleGenerator $generator, 
+        GetParam $getParam, 
+        CycleRepository $cycleRepository
+    ): Response
+    {
+        $paramCycleStart = DateTimeImmutable::createFromFormat('Y-m-d', $getParam->get('CYCLE_START'));
+
+        if( !$paramCycleStart ) {
+            $this->addFlash('error', 'Le pamatrètre CYCLE_START n\'a pas été trouvé ou n\'est pas au bon format AAAA-MM-DD');
+            return $this->redirectToRoute('admin_cycle_index');
+        }
+
+        $paramCycleNbWeeks = (int) $getParam->get('CYCLE_NB_WEEKS');
+        
+        if( !($paramCycleNbWeeks>0) ) {
+            $this->addFlash('error', 'Le pamatrètre CYCLE_NB_WEEKS n\'a pas été trouvé ou n\'est pas supérieur à 0');
+            return $this->redirectToRoute('admin_cycle_index');
+        }
+
+        $lastCycleFound = $cycleRepository->findLast();
+
+        // Find next Cycles to generate
+        $lastFinishDate = $paramCycleStart->modify('-1 day');
+        if (null !== $lastCycleFound) {
+            $lastFinishDate = $lastCycleFound->getFinish();
+        }
+        $nextStartDate = $lastFinishDate->modify('+1 day');
+        $nextFinishDate = $nextStartDate->modify('+'.$paramCycleNbWeeks.' weeks - 1 day');
+        
+        if ($this->isCsrfTokenValid('generate-cycle'.$nextStartDate->format('Ymd'), $request->request->get('_token'))) {
+            
+            try {
+                $generator->generate($nextStartDate, $nextFinishDate);
+                $this->addFlash('success', 'Le nouveau cycle a été généré');
+                return $this->redirectToRoute('admin_cycle_index');
+            } catch(Exception $e) {
+                $this->addFlash('error', $e->getMessage());
+            }
+            
+        }
+
+        return $this->renderForm('admin/cycle/week_generator.html.twig', [
+            'nextStartDate' => $nextStartDate,
+            'nextFinishDate' => $nextFinishDate,
+        ]);
+    }
 }
